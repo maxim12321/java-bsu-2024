c4ea2e1307be68016565df60769594bde03a4b1f
Zorka Matsvei <matwejzorka2005@gmail.com>
2024-10-07 14:57:30 +0300

    Adding basic files and contents

---
+++ by/MatsveiZorka/quizer/Task.java
@@ -0,0 +1,17 @@
+package by.MatsveiZorka.quizer;
+
+public interface Task {
+    /**
+     @return текст задания
+     */
+    String getText();
+
+    /**
+     * Проверяет ответ на задание и возвращает результат
+     *
+     * @param  answer ответ на задание
+     * @return        результат ответа
+     * @see           Result
+     */
+    Result validate(String answer);
+}
---
+++ by/MatsveiZorka/quizer/TaskGenerator.java
@@ -0,0 +1,11 @@
+package by.MatsveiZorka.quizer;
+
+public interface TaskGenerator {
+    /**
+     * Возвращает задание. При этом новый объект может не создаваться, если класс задания иммутабельный
+     *
+     * @return задание
+     * @see    Task
+     */
+    Task generate();
+}
