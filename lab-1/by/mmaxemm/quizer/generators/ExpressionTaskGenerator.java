9ea3439c927dd492cf92aff22ecf00dd21937f99
Maksim Hindzerau lin <gindzeraumaksim@gmail.com>
2024-10-20 22:19:09 +0300

    ExpressionTask, ExpressionTaskGenerator

--- lab-1/by/mmaxemm/quizer/generators/ExpressionTaskGenerator.java
+++ lab-1/by/mmaxemm/quizer/generators/ExpressionTaskGenerator.java
@@ -1,17 +1,16 @@
 package by.mmaxemm.quizer.generators;
 
+import by.mmaxemm.quizer.Task;
 import by.mmaxemm.quizer.tasks.ExpressionTask;
 import by.mmaxemm.quizer.TaskGenerator;
 
+import java.util.ArrayList;
+import java.util.Random;
 public class ExpressionTaskGenerator implements TaskGenerator {
-    /**
-     * @param minNumber              минимальное число
-     * @param maxNumber              максимальное число
-     * @param generateSum            разрешить генерацию с оператором +
-     * @param generateDifference     разрешить генерацию с оператором -
-     * @param generateMultiplication разрешить генерацию с оператором *
-     * @param generateDivision       разрешить генерацию с оператором /
-     */
+    int minNumber;
+    int maxNumber;
+    ArrayList<String> operators;
+
     ExpressionTaskGenerator(
             int minNumber,
             int maxNumber,
@@ -20,15 +19,40 @@ public class ExpressionTaskGenerator implements TaskGenerator {
             boolean generateMultiplication,
             boolean generateDivision
     ) {
-        // ...
-    }
+        if(minNumber > maxNumber) {
+            throw new IllegalArgumentException("minNumber must be less or equal to maxNumber");
+        }
+        if(!(generateSum
+                || generateDifference
+                || generateMultiplication
+                || generateDivision)) {
+            throw new IllegalArgumentException("At least one operation must be enabled");
+        }
 
-    /**
-     * return задание типа {@link ExpressionTask}
-     */
+        this.minNumber = minNumber;
+        this.maxNumber = maxNumber;
+        operators = new ArrayList<String>();
+        if(generateSum) {
+            operators.add("+");
+        }
+        if(generateDifference) {
+            operators.add("-");
+        }
+        if(generateMultiplication) {
+            operators.add("*");
+        }
+        if(generateDivision) {
+            operators.add("/");
+        }
+    }
 
     @Override
     public ExpressionTask generate() {
-        return null;
+        Random randomizer = new Random();
+        int num1 = randomizer.nextInt(maxNumber - minNumber + 1) + minNumber;
+        int num2 = randomizer.nextInt(maxNumber - minNumber + 1) + minNumber;
+        int operatorIndex = randomizer.nextInt(operators.size());
+        String operator = operators.get(operatorIndex);
+        return new ExpressionTask(num1, num2, operator);
     }
 }
