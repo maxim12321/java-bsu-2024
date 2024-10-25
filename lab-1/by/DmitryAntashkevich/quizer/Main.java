9786c8271d97040f5a1964db3897099daf4b1039
Dmi34 <antashkevich.dima@yandex.ru>
2024-10-26 02:06:56 +0300

    Add: Quiz and simple test

--- lab-1/by/DmitryAntashkevich/quizer/Main.java
+++ lab-1/by/DmitryAntashkevich/quizer/Main.java
@@ -1,22 +1,54 @@
 package by.DmitryAntashkevich.quizer;
 
-import by.DmitryAntashkevich.quizer.tasks.math.MathTask;
+import by.DmitryAntashkevich.quizer.generators.math.ExpressionTaskGenerator;
+import by.DmitryAntashkevich.quizer.tasks.math.MathTask.Operation;
 
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     /**
      * @return тесты в {@link Map}, где
-     * ключ    - название теста {@link String}
+     * ключ - название теста {@link String}
      * значение - сам тест       {@link Quiz}
      */
-//    static Map<String, Quiz> getQuizMap() {
-//        // ...
-//    }
+    static Map<String, Quiz> getQuizMap() {
+        Map<String, Quiz> quizMap = new HashMap<>();
+
+        TaskGenerator simpleExpressionGenerator = new ExpressionTaskGenerator(0, 100, EnumSet.of(Operation.ADDITION, Operation.SUBTRACTION));
+        quizMap.put("simple_expressions", new Quiz(simpleExpressionGenerator, 5));
+
+        return quizMap;
+    }
 
     public static void main(String[] args) {
+        var quizMap = getQuizMap();
+        System.out.println("Доступные тесты:");
+        for (String key : quizMap.keySet()) {
+            System.out.println(key);
+        }
+
+        System.out.println("Введите название теста...");
+        Scanner in = new Scanner(System.in);
+        String quizName;
+        while (true) {
+            quizName = in.next();
+            if (quizMap.containsKey(quizName)) {
+                break;
+            }
+            System.out.println("Теста с данным названием не существует. Введите название теста из списка выше...");
+        }
+        Quiz quiz = quizMap.get(quizName);
+
+        while (!quiz.isFinished()) {
+            Task task = quiz.nextTask();
+            System.out.println(task.getText());
+            final String answer = in.next();
+            System.out.println(switch (quiz.provideAnswer(answer)) {
+                case OK -> "Верный ответ!";
+                case WRONG -> "Неверный ответ!";
+                case INCORRECT_INPUT -> "Некорректный формат ответа. Попробуйте еще раз...";
+            });
+        }
+        System.out.printf("Ваша оценка: %.2f\n", quiz.getMark());
     }
 }
--- lab-1/by/DmitryAntashkevich/quizer/Quiz.java
+++ lab-1/by/DmitryAntashkevich/quizer/Quiz.java
@@ -9,58 +9,86 @@ public class Quiz {
      * @param taskCount количество заданий в тесте
      */
     Quiz(TaskGenerator generator, int taskCount) {
-        // ...
+        this.generator = generator;
+        this.taskCount = taskCount;
     }
 
     /**
-     * @return задание, повторный вызов вернет слелующее
+     * @return задание, повторный вызов вернет следующее
      * @see Task
      */
     Task nextTask() {
-        // ...
+        if (currentTask == null) {
+            currentTask = generator.generate();
+        }
+        return currentTask;
     }
 
     /**
-     * Предоставить ответ ученика. Если результат {@link Result#INCORRECT_INPUT}, то счетчик неправильных 
+     * Предоставить ответ ученика. Если результат {@link Result#INCORRECT_INPUT}, то счетчик неправильных
      * ответов не увеличивается, а {@link #nextTask()} в следующий раз вернет тот же самый объект {@link Task}.
      */
     Result provideAnswer(String answer) {
-        // ...
+        if (currentTask == null) {
+            throw new RuntimeException("Attempt to answer a non-existent question");
+        }
+        Result result = currentTask.validate(answer);
+        switch (result) {
+            case OK -> correctAnswerCount++;
+            case WRONG -> wrongAnswerCount++;
+            case INCORRECT_INPUT -> incorrectAnswerCount++;
+        }
+        if (!result.equals(Result.INCORRECT_INPUT)) {
+            currentTask = null;
+            completedTaskCount++;
+        }
+        return result;
     }
 
     /**
      * @return завершен ли тест
      */
     boolean isFinished() {
-        // ...
+        return completedTaskCount == taskCount;
     }
 
     /**
      * @return количество правильных ответов
      */
-    int getCorrectAnswerNumber() {
-        // ...
+    public int getCorrectAnswerNumber() {
+        return correctAnswerCount;
     }
 
     /**
      * @return количество неправильных ответов
      */
-    int getWrongAnswerNumber() {
-        // ...
+    public int getWrongAnswerNumber() {
+        return wrongAnswerCount;
     }
 
     /**
      * @return количество раз, когда был предоставлен неправильный ввод
      */
-    int getIncorrectInputNumber() {
-        // ...
+    public int getIncorrectInputNumber() {
+        return incorrectAnswerCount;
     }
 
     /**
-     * @return оценка, которая является отношением количества правильных ответов к количеству всех вопросов. 
-     *         Оценка выставляется только в конце!
+     * @return оценка, которая является отношением количества правильных ответов к количеству всех вопросов.
+     * Оценка выставляется только в конце!
      */
-    double getMark() {
-        // ...
+    public double getMark() {
+        if (!isFinished()) {
+            throw new RuntimeException("Test is not finished yet");
+        }
+        return (double) correctAnswerCount / (double) taskCount;
     }
+
+    private final TaskGenerator generator;
+    private final int taskCount;
+    private int completedTaskCount = 0;
+    private int correctAnswerCount = 0;
+    private int wrongAnswerCount = 0;
+    private int incorrectAnswerCount = 0;
+    private Task currentTask = null;
 }
