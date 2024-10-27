baa303ea38092e2df93244a9b7cf70e511fd74f3
10-13 <103519265+10-13@users.noreply.github.com>
2024-10-27 23:07:11 +0300

    Fixed few warnings

---
+++ by/v10k13/quizer/README.md
@@ -0,0 +1,8 @@
+### Tests
+For more comfort testing where created tester class.
+
+To check tests run:
+
+```java
+by.v10k13.quizer.tests.tester.TestMain.main
+```
--- by/v10k13/quizer/tests/SampleMain.java
+++ by/v10k13/quizer/tests/SampleMain.java
@@ -5,11 +5,13 @@ import by.v10k13.quizer.Task;
 import by.v10k13.quizer.TaskGenerator;
 import by.v10k13.quizer.generators.GroupTaskGenerator;
 import by.v10k13.quizer.generators.PoolTaskGenerator;
+import by.v10k13.quizer.generators.math.AbstractMathTaskGenerator;
+import by.v10k13.quizer.generators.math.EquationTaskGenerator;
+import by.v10k13.quizer.generators.math.ExpressionTaskGenerator;
 import by.v10k13.quizer.tasks.TextTask;
+import by.v10k13.quizer.tasks.math.MathTask;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class SampleMain {
 
@@ -24,7 +26,7 @@ public class SampleMain {
             list.add(new TextTask("How are you?", "OK"));
             list.add(new TextTask("How are you?", "OK!"));
             var Gen = new PoolTaskGenerator(false, list);
-            res.put("PoolGenerator", new Quiz(Gen, 6));
+            res.put("PoolTaskGenerator", new Quiz(Gen, 6));
         }
         {
             var gen_list = new ArrayList<TaskGenerator<?>>();
@@ -51,12 +53,29 @@ public class SampleMain {
                 gen_list.add(Gen);
             }
             var Gen = new GroupTaskGenerator(gen_list);
-            res.put("PoolGenerator", new Quiz(Gen, 6));
+            res.put("GroupTaskGenerator", new Quiz(Gen, 12));
+        }
+        {
+            var conf = new AbstractMathTaskGenerator.MathTaskGeneratorConfig(1, 10, EnumSet.allOf(MathTask.Operators.class));
+            res.put("ExpressionTaskGenerator", new Quiz(new ExpressionTaskGenerator(conf), 6));
+        }
+        {
+            var conf = new AbstractMathTaskGenerator.MathTaskGeneratorConfig(1, 10, EnumSet.allOf(MathTask.Operators.class));
+            res.put("EquationTaskGenerator", new Quiz(new EquationTaskGenerator(conf), 6));
         }
         return res;
     }
 
     public static void main(String[] args) {
-
+        var q =getQuizMap().get(args.length > 0 ? args[0] : "GroupTaskGenerator");
+        Scanner scan = new Scanner(System.in);
+        while (!q.isFinished()) {
+            System.out.println(q.nextTask().getText());
+            var ans = scan.next();
+            var res = q.provideAnswer(ans);
+            if (res == Task.Result.INCORRECT_INPUT)
+                System.out.println("Incorrect input.");
+        }
+        System.out.println(q.getMark());
     }
 }
