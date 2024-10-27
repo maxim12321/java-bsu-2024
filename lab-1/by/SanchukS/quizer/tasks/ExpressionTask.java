c17911142155f2ae94f328bc6cd6c4fb07a53fec
SanchukS <SanchukS2005@gmail.com>
2024-10-28 00:00:12 +0300

    ExpressionTask updated using AbstractMathTask

--- lab-1/by/SanchukS/quizer/tasks/ExpressionTask.java
+++ lab-1/by/SanchukS/quizer/tasks/ExpressionTask.java
@@ -3,31 +3,10 @@ package by.SanchukS.quizer.tasks;
 import by.SanchukS.quizer.Expression;
 import by.SanchukS.quizer.Result;
 import by.SanchukS.quizer.Task;
+import by.SanchukS.quizer.tasks.math.AbstractMathTask;
 
-public class ExpressionTask implements Task {
-    Expression expression;
-
-
-    public ExpressionTask(Expression expression) {
-        this.expression = expression;
-    }
-
-    @Override
-    public String getText() {
-        return expression.getNumber(0)
-                + expression.getOperation()
-                + expression.getNumber(1)
-                + "=?";
-    }
-
-    @Override
-    public Result validate(String answer) {
-        int rightAnswer = expression.getNumber(2);
-        try {
-            int userAnswer = Integer.parseInt(answer);
-            return userAnswer == rightAnswer ? Result.OK : Result.WRONG;
-        } catch (NumberFormatException e) {
-            return Result.INCORRECT_INPUT;
-        }
+public class ExpressionTask extends AbstractMathTask {
+    public ExpressionTask(Expression e) {
+        super(e.getNumber(0) + e.getOperation() + e.getNumber(1), e.getNumber(2));
     }
 }
