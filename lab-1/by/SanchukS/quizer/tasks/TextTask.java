81613015db4f102804ad368e92ed977ebe2d4290
SanchukS <SanchukS2005@gmail.com>
2024-10-27 23:09:12 +0300

    TextTask realized

---
+++ lab-1/by/SanchukS/quizer/tasks/TextTask.java
@@ -0,0 +1,35 @@
+package by.SanchukS.quizer.tasks;
+
+import by.SanchukS.quizer.Result;
+import by.SanchukS.quizer.Task;
+
+/**
+ * Задание с заранее заготовленным текстом.
+ * Можно использовать {@link PoolTaskGenerator}, чтобы задавать задания такого типа.
+ */
+class TextTask implements Task {
+    private final String text;
+    private final String answer;
+
+    /**
+     * @param text   текст задания
+     * @param answer ответ на задание
+     */
+    TextTask(
+            String text,
+            String answer
+    ) {
+        this.text = text;
+        this.answer = answer;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public Result validate(String answer) {
+        return this.answer.equals(answer) ? Result.OK : Result.WRONG;
+    }
+}
