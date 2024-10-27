6c1e16e2a4bc1b8e14615499a3b1f346006d2147
SanchukS <SanchukS2005@gmail.com>
2024-10-27 21:14:49 +0300

    ExpressionTask updated using Expression

--- lab-1/by/SanchukS/quizer/tasks/ExpressionTask.java
+++ lab-1/by/SanchukS/quizer/tasks/ExpressionTask.java
@@ -1,37 +1,30 @@
 package by.SanchukS.quizer.tasks;
 
+import by.SanchukS.quizer.Expression;
 import by.SanchukS.quizer.Result;
 import by.SanchukS.quizer.Task;
 
 public class ExpressionTask implements Task {
-    private final int firstNumber;
-    private final int secondNumber;
-    private final String operation;
-    private final int answer;
+    Expression expression;
 
 
-    public ExpressionTask(int firstNumber, String operation, int secondNumber) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        this.operation = operation;
-        this.answer = switch(operation) {
-            case "+" -> this.firstNumber + this.secondNumber;
-            case "-" -> this.firstNumber - this.secondNumber;
-            case "*" -> this.firstNumber * this.secondNumber;
-            case "/" -> this.firstNumber / this.secondNumber;
-            default -> throw new IllegalArgumentException();
-        };
+    public ExpressionTask(Expression expression) {
+        this.expression = expression;
     }
 
     @Override
     public String getText() {
-        return firstNumber + " " + operation + " " + secondNumber + " = ?";
+        return expression.getNumber(0)
+                + expression.getOperation()
+                + expression.getNumber(1)
+                + "=?";
     }
 
     @Override
     public Result validate(String answer) {
         try {
-            return Integer.parseInt(answer) == this.answer ? Result.OK : Result.WRONG;
+            int userAnswer = Integer.parseInt(answer);
+            return userAnswer == expression.getNumber(2) ? Result.OK : Result.WRONG;
         } catch (NumberFormatException e) {
             return Result.INCORRECT_INPUT;
         }
