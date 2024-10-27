0d5bea5a2131128c02946e2f097987e1bc67540f
SanchukS <SanchukS2005@gmail.com>
2024-10-27 23:58:11 +0300

    EquationTask updated using AbstractMathClass (extends)

--- lab-1/by/SanchukS/quizer/tasks/EquationTask.java
+++ lab-1/by/SanchukS/quizer/tasks/EquationTask.java
@@ -2,42 +2,13 @@ package by.SanchukS.quizer.tasks;
 
 import by.SanchukS.quizer.Expression;
 import by.SanchukS.quizer.Result;
-import by.SanchukS.quizer.Task;
+import by.SanchukS.quizer.tasks.math.AbstractMathTask;
 
-public class EquationTask implements Task {
-    Expression expression;
-    private final boolean variableRight;
-
-    public EquationTask(Expression expression, boolean variableRight) {
-        this.expression = expression;
-        this.variableRight = variableRight;
-    }
-
-    @Override
-    public String getText() {
-        if (variableRight) {
-            return expression.getNumber(0)
-                    + expression.getOperation()
-                    + "x="
-                    + expression.getNumber(2);
-        } else {
-            return "x"
-                    + expression.getOperation()
-                    + expression.getNumber(1)
-                    + "="
-                    + expression.getNumber(2);
-        }
-
-    }
-
-    @Override
-    public Result validate(String answer) {
-        int rightAnswer = variableRight ? expression.getNumber(1) : expression.getNumber(0);
-        try {
-            int userAnswer = Integer.parseInt(answer);
-            return userAnswer == rightAnswer ? Result.OK : Result.WRONG;
-        } catch (NumberFormatException e) {
-            return Result.INCORRECT_INPUT;
-        }
-    }
+public class EquationTask extends AbstractMathTask {
+    public EquationTask(Expression e, boolean variableRight) {
+        super(variableRight ? e.getNumber(0) + e.getOperation() + "x=" + e.getNumber(2)
+                : "x" + e.getOperation() + e.getNumber(1) + "=" + e.getNumber(2),
+              variableRight ? e.getNumber(1) : e.getNumber(0)
+        );
+    };
 }
