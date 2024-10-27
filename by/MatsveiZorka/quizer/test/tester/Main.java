403b307c1cf87b051b0b110703927aee31b6f1f7
Zorka Matsvei <matwejzorka2005@gmail.com>
2024-10-27 20:32:19 +0300

    Adding basics generators. Improving few features.

--- by/MatsveiZorka/quizer/test/tester/Main.java
+++ by/MatsveiZorka/quizer/test/tester/Main.java
@@ -1,10 +1,18 @@
 package by.MatsveiZorka.quizer.test.tester;
 
+import by.MatsveiZorka.quizer.Quiz;
+import java.util.Map;
+import java.util.HashMap;
+
 
 public class Main {
 
     public static void main(String[] args) {
         Tester.RunGroups(Main.class);
+    }
+
+    public static Map<String, Quiz> getQuizMap() {
+        return null;
     }
 
     @TestableGroup(Name="Creating tests.")
