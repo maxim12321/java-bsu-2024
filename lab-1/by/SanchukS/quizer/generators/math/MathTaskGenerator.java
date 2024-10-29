5f7abcf95b76aa9377c6188bf3c9307c56ef9650
SanchukS <SanchukS2005@gmail.com>
2024-10-29 18:29:43 +0300

    MathTaskGenerator: getters added

--- lab-1/by/SanchukS/quizer/generators/math/MathTaskGenerator.java
+++ lab-1/by/SanchukS/quizer/generators/math/MathTaskGenerator.java
@@ -3,4 +3,13 @@ package by.SanchukS.quizer.generators.math;
 import by.SanchukS.quizer.TaskGenerator;
 
 public interface MathTaskGenerator extends TaskGenerator {
+    public int getMinNumber(); // получить минимальное число
+    public int getMaxNumber(); // получить максимальное число
+
+    /**
+     * @return разница между максимальным и минимальным возможным числом
+     */
+    default int getDiffNumber() {
+        return getMaxNumber() - getMinNumber();
+    }
 }
